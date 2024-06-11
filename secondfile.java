fd1088130d9d5aac83623e0e77e9bbfbb940a961
radhakrishna gajarla <rkgajarla@gmail.com>
2024-06-11 17:59:19 +0530

    this is second file

---
+++ secondfile.java
@@ -0,0 +1,11 @@
+this is second ffile
+babai
+you have to first complete this than go for other
+be organic
+be natural
+be present
+be there
+be calm with the family members
+be supportive with the family members
+be patient
+be understable
